f8228951dd716758c40ee08c937846a9ddec0cc4
ujjwalgupta94 <80671501+ujjwalgupta94@users.noreply.github.com>
2022-01-21 16:28:26 +0530

    add matic_token

---
+++ src/contracts/matic_token.ts
@@ -0,0 +1,54 @@
+import { BaseToken, Converter, IPOSClientConfig, MAX_AMOUNT, TYPE_AMOUNT, Web3SideChainClient } from "@maticnetwork/maticjs";
+
+export class MaticToken extends BaseToken<IPOSClientConfig> {
+
+    stakeManagerAddress: string;
+
+    constructor(client: Web3SideChainClient<IPOSClientConfig>, address: string, stakeManagerAddress: string) {
+        super(
+            {
+                isParent: true,
+                address: address,
+                name: "MaticToken",
+                bridgeType: 'plasma'
+            },
+            client
+        );
+        this.stakeManagerAddress = stakeManagerAddress;
+    }
+
+    protected getMethod(name: string, ...args) {
+        return this.getContract().then(contract => {
+            return contract.method(name, ...args);
+        })
+    }
+
+    getAllowanceForStakingManager(userAddress: string) {
+        return this.getMethod(
+            "allowance",
+            userAddress,
+            this.stakeManagerAddress
+        ).then(method => {
+            return this.processWrite(method);
+        })
+    }
+
+    approveMaxForStaking() {
+        return this.getMethod(
+            "approve",
+            this.stakeManagerAddress,
+            MAX_AMOUNT
+        ).then(method => {
+            return this.processWrite(method);
+        })
+    }
+
+    getBalance(userAddress: string) {
+        return this.getMethod(
+            "balanceOf",
+            userAddress,
+        ).then(method => {
+            return this.processRead(method);
+        })
+    }
+}
